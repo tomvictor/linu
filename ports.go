8f08fcda759d955ac28356a1a6118a24c45c73d2
Tom Victor <vjtomvictor@icloud.com>
2023-07-17 17:01:48 +0200

    Update ports.go

--- ports.go
+++ ports.go
@@ -5,10 +5,12 @@ import (
 	"os/exec"
 )
 
+var BaudRate string = "115200"
+
 func openPort(port string) {
 	fmt.Println("Opening port: ", port)
 	cmd := exec.Command("osascript", "-e",
-		fmt.Sprintf(`tell app "Terminal" to do script "screen %s 115200"`, port))
+		fmt.Sprintf(`tell app "Terminal" to do script "minicom -b %s -D %s"`, BaudRate, port))
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println("failed to run screen")
